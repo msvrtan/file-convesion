df9ba375c397c078e3fd40136034f325ac871350
Miro Svrtan <miro@nuldevelopment.hr>
2026-04-04 15:41:44 +0200

    chore: configuring php-cs-fixer

--- _prompt.md
+++ _prompt.md
@@ -15,3 +15,10 @@ please set up phpstan to be extra strict and use all of the installed extensions
 ## 2026-04-04T15:35:29+02:00 [gpt-5.4 high]
 please create phpstan baseline file
 
+## 2026-04-04T15:37:41+02:00 [gpt-5.4 high]
+please set up cs-fixer config so it runs all psr and symfony standards + strict types need to be declared everywhere
+
+
+## 2026-04-04T15:40:22+02:00 [gpt-5.4 high]
+instead of defining whole project, please run php-cs-fixer over src and tests folders only
+
--- api/.php-cs-fixer.dist.php
+++ api/.php-cs-fixer.dist.php
@@ -1,17 +1,20 @@
 <?php
 
 $finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
-    ->exclude('var')
-    ->notPath([
-        'config/bundles.php',
-        'config/reference.php',
+    ->in([
+        __DIR__.'/src',
+        __DIR__.'/tests',
     ])
 ;
 
 return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
     ->setRules([
+        '@PSR1' => true,
+        '@PSR2' => true,
+        '@PSR12' => true,
         '@Symfony' => true,
+        'declare_strict_types' => true,
     ])
     ->setFinder($finder)
 ;
