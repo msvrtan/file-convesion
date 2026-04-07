934385d9f14f8af68551be6a3e80af49761ce3c8
Miro Svrtan <miro@nuldevelopment.hr>
2026-04-07 15:11:11 +0200

    Add conversion route method coverage

---
+++ api/tests/Functional/ConversionRoutingTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use App\DataFixtures\AppFixtures;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+final class ConversionRoutingTest extends WebTestCase
+{
+    use AuthenticatesCustomer;
+
+    private KernelBrowser $client;
+
+    protected function setUp(): void
+    {
+        self::ensureKernelShutdown();
+        $this->client = self::createClient();
+    }
+
+    protected function browser(): KernelBrowser
+    {
+        return $this->client;
+    }
+
+    public function testListEndpointRejectsGetMethod(): void
+    {
+        $token = $this->createJwtToken(AppFixtures::ACME_USERNAME);
+
+        $this->client->request(
+            'GET',
+            '/conversions',
+            server: [
+                'HTTP_AUTHORIZATION' => sprintf('Bearer %s', $token),
+            ],
+        );
+
+        self::assertResponseStatusCodeSame(Response::HTTP_METHOD_NOT_ALLOWED);
+        self::assertResponseHeaderSame('allow', 'POST');
+    }
+
+    public function testStatusEndpointRejectsPostMethod(): void
+    {
+        $token = $this->createJwtToken(AppFixtures::ACME_USERNAME);
+
+        $this->client->request(
+            'POST',
+            '/conversions/019d86b0-0000-7000-8000-000000000001',
+            server: [
+                'HTTP_AUTHORIZATION' => sprintf('Bearer %s', $token),
+            ],
+        );
+
+        self::assertResponseStatusCodeSame(Response::HTTP_METHOD_NOT_ALLOWED);
+        self::assertResponseHeaderSame('allow', 'GET');
+    }
+}
